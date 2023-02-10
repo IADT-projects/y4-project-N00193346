1c8890af826ddd0fa7c7e975b3d941de6d58306a
Paul Doyle <pauldoyle91@gmail.com>
2023-02-10 22:57:18 +0000

    Have basic video call app working on local server, using firestone to make intial connection

--- Source/guitar-tutor/src/App.jsx
+++ Source/guitar-tutor/src/App.jsx
@@ -1,5 +1,13 @@
-import "./App.css";
 import styled from "styled-components";
+
+import { useState } from "react";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+import "firebase/firestore";
+
+import Menu from "./components/Menu";
+import Videos from "./components/Videos";
 
 export const VideoContainer = styled.div`
   display: grid;
@@ -13,38 +21,47 @@ export const VideoStream = styled.video`
   height: 300px;
 `;
 
+const firebaseConfig = {
+  apiKey: "AIzaSyAELfHh4mgVaAuqujhydOpoMhx5YrwWKRQ",
+  authDomain: "testing-video-bf1a8.firebaseapp.com",
+  projectId: "testing-video-bf1a8",
+  storageBucket: "testing-video-bf1a8.appspot.com",
+  messagingSenderId: "806540157505",
+  appId: "1:806540157505:web:05d2f36e25a3f3eb695cc0",
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
 function App() {
-  let localStream;
-  let remoteStream;
-
-  let init = async () => {
-    localStream = await navigator.mediaDevices.getUserMedia({
-      video: true,
-      audio: true,
-    });
-    document.getElementById("user-1").srcObject = localStream;
-  };
-
-  init();
+  //Change Page
+  const [currentPage, setCurrentPage] = useState("home");
+  //Code used to answer call
+  const [joinCode, setJoinCode] = useState("");
 
   return (
-    <>
-      <VideoContainer>
-        <VideoStream
-          class="video-player"
-          id="user-1"
-          autoPlay
-          playsInline
-        ></VideoStream>
-        <VideoStream
-          class="video-player"
-          id="user-2"
-          autoPlay
-          playsInline
-        ></VideoStream>
-      </VideoContainer>
-    </>
+    <div className="app">
+      {currentPage === "home" ? (
+        <Menu
+          joinCode={joinCode}
+          setJoinCode={setJoinCode}
+          setPage={setCurrentPage}
+        />
+      ) : (
+        <Videos mode={currentPage} callId={joinCode} setPage={setCurrentPage} />
+      )}
+    </div>
   );
+
+  // return (
+  //   <>
+  //     <VideoContainer>
+  //       <VideoStream id="user-1" autoPlay playsInline></VideoStream>
+  //       <VideoStream id="user-2" autoPlay playsInline></VideoStream>
+  //     </VideoContainer>
+  //   </>
+  // );
 }
 
 export default App;
