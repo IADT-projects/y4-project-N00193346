59db517e5cc0f7d8f3d60c8ff2758f7bb6630570
Paul Doyle <pauldoyle91@gmail.com>
2023-03-11 01:31:10 +0000

    Set up SocketContext to handle multiple signal events

--- Source/guitar-tutor/guitar-rebuild/client/src/App.js
+++ Source/guitar-tutor/guitar-rebuild/client/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Typography from "@mui/material/Typography";
+import AppBar from "@mui/material/AppBar";
+import styled from "styled-components";
 
-function App() {
+import Videos from "./components/Videos";
+import Options from "./components/Options";
+import Notifications from "./components/Notifications";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          Video Chat
+        </Typography>
+      </AppBar>
+
+      <Videos />
+      <Options>
+        <Notifications />
+      </Options>
     </div>
   );
-}
+};
 
 export default App;
