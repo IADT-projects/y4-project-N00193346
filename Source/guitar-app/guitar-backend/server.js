629c881d14b802ba6f448168b1075ffe8c1f23f7
Paul Doyle <pauldoyle91@gmail.com>
2023-03-23 01:00:52 +0000

    Socket IO Implementation
    - Users can send and receive friend requests
    - Users can accept/reject friend requests
    - Friend's list updated in UI
    - Online friends show in friend's list

--- Source/guitar-app/guitar-backend/server.js
+++ Source/guitar-app/guitar-backend/server.js
@@ -4,7 +4,9 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
+const socketServer = require("./socketServer");
 const authRoutes = require("./routes/authRoutes");
+const friendInvitationRoutes = require("./routes/friendInvitationRoutes");
 
 const PORT = process.env.PORT || process.env.API_PORT;
 
@@ -13,8 +15,10 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/friend-invitation", friendInvitationRoutes);
 
 const server = http.createServer(app);
+socketServer.registerSocketServer(server);
 
 mongoose
   .connect(process.env.MONGO_URL)
