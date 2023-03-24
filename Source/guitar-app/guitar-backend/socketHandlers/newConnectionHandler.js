6d21d9ee03b48e980247e97d40a29a0d6b9e8bd2
Paul Doyle <pauldoyle91@gmail.com>
2023-03-24 17:43:09 +0000

    WebRTC Implementation
    - Users can create rooms
    - Friends of users can see and join their friend's rooms
    - Users can join rooms and begin peer connection
    - Users share video and audio data
    - Users can share screen
    - User's and mute audio and video

--- Source/guitar-app/guitar-backend/socketHandlers/newConnectionHandler.js
+++ Source/guitar-app/guitar-backend/socketHandlers/newConnectionHandler.js
@@ -1,5 +1,6 @@
 const serverStore = require("../serverStore");
 const friendsUpdate = require("../socketHandlers/updates/friends");
+const roomsUpdate = require("./updates/rooms");
 
 const newConnectionHandler = async (socket, io) => {
   const userDetails = socket.user;
@@ -12,8 +13,12 @@ const newConnectionHandler = async (socket, io) => {
   // update pending friends invitations list
   friendsUpdate.updateFriendsPendingInvitations(userDetails.userId);
 
-  // // update friends list
+  // update friends list
   friendsUpdate.updateFriends(userDetails.userId);
+
+  setTimeout(() => {
+    roomsUpdate.updateRooms(socket.id);
+  }, [500]);
 };
 
 module.exports = newConnectionHandler;
