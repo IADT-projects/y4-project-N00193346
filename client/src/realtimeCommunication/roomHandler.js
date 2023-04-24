cd21b2c6614f7fe4053d3f4383018a57e5bbf527
Paul Doyle <pauldoyle91@gmail.com>
2023-04-24 20:14:36 +0100

    Report: Added room functionality to implementations section

--- client/src/realtimeCommunication/roomHandler.js
+++ client/src/realtimeCommunication/roomHandler.js
@@ -31,18 +31,23 @@ export const newRoomCreated = (data) => {
 
 export const updateActiveRooms = (data) => {
   const { activeRooms } = data;
-
+  //Get friends of user
   const friends = store.getState().friends.friends;
   const rooms = [];
 
+  //Get User Id
   const userId = store.getState().auth.userDetails?._id;
 
+  //Go through all rooms
   activeRooms.forEach((room) => {
+    //Check if user created the room
     const isRoomCreatedByMe = room.roomCreator.userId === userId;
 
     if (isRoomCreatedByMe) {
+      //Show Room named as "Me" if user created
       rooms.push({ ...room, creatorUsername: "Me" });
     } else {
+      //Show Room with friend name if friend created
       friends.forEach((f) => {
         if (f.id === room.roomCreator.userId) {
           rooms.push({ ...room, creatorUsername: f.username });
@@ -68,24 +73,35 @@ export const joinRoom = (roomId) => {
 };
 
 export const leaveRoom = () => {
+  //Get room Id from redux
   const roomId = store.getState().room.roomDetails.roomId;
 
+  //Check if user has a local stream
   const localStream = store.getState().room.localStream;
   if (localStream) {
+    //Stop all tracks
     localStream.getTracks().forEach((track) => track.stop());
+    //Set to null
     store.dispatch(setLocalStream(null));
   }
 
+  //Check if user sharing screen
   const screenSharingStream = store.getState().room.screenSharingStream;
   if (screenSharingStream) {
+    //Stop all tracks
     screenSharingStream.getTracks().forEach((track) => track.stop());
     store.dispatch(setScreenSharingStream(null));
   }
 
+  // Set remote streams to empty array
   store.dispatch(setRemoteStreams([]));
+  // Close peer connections
   webRTCHandler.closeAllConnections();
 
+  //Leave room
   socketConnection.leaveRoom({ roomId });
+  //Set room details to null
   store.dispatch(setRoomDetails(null));
+  //Remove user from room
   store.dispatch(setOpenRoom(false, false));
 };
